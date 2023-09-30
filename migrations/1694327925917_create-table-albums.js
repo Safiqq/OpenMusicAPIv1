cb1150dc1d85513f1199eb6d8d14babbc6116cce
safiqq <szarifin20041@gmail.com>
2023-09-30 23:21:14 +0700

    chore: rapiin linter

--- migrations/1694327925917_create-table-albums.js
+++ migrations/1694327925917_create-table-albums.js
@@ -1,28 +1,28 @@
-exports.up = pgm => {
-    pgm.createTable('albums', {
-        id: {
-            type: 'varchar(50)',
-            primaryKey: true,
-        },
-        name: {
-            type: 'text',
-            notNull: true,
-        },
-        year: {
-            type: 'integer',
-            notNull: true,
-        },
-        created_at: {
-            type: 'text',
-            notNull: true,
-        },
-        updated_at: {
-            type: 'text',
-            notNull: true,
-        },
-    });
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'varchar(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'text',
+      notNull: true,
+    },
+    year: {
+      type: 'integer',
+      notNull: true,
+    },
+    created_at: {
+      type: 'text',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'text',
+      notNull: true,
+    },
+  });
 };
 
-exports.down = pgm => {
-    pgm.dropTable('albums');
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
 };
